3801fa9035320472a7b073dfed97c5aa3af8e6b4
seesharply <seesharply@outlook.com>
2018-06-28 15:34:21 +0800

    change language type

--- Vsixprop/Properties/AssemblyInfo.cs
+++ Vsixprop/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Vsixprop")]
+[assembly: AssemblyTitle("PropEntity")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Vsixprop")]
-[assembly: AssemblyCopyright("")]
+[assembly: AssemblyCompany("weway software")]
+[assembly: AssemblyProduct("PropEntity")]
+[assembly: AssemblyCopyright("seesharply@outlook.com")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
